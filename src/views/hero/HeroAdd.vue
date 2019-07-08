32e6a874cfc8f4cd34f018eabd4cbb9c21cf0b2e
kid <1051716213@qq.com>
2019-07-08 22:37:58 +0800

    添加功能

--- src/views/hero/HeroAdd.vue
+++ src/views/hero/HeroAdd.vue
@@ -4,23 +4,56 @@
     <form>
       <div class="form-group">
         <label for="txtName">姓名</label>
-        <input type="text" class="form-control" id="txtName" placeholder="姓名" />
-         <div class="form-group">
-            <label for="exampleInputPassword1">性别</label>
-            <!-- <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"> -->
-            <select id="gender"  class="form-control">
-              <option value="男">男</option>
-              <option value="女">女</option>
-            </select>
-          </div>
+        <input type="text" class="form-control" v-model="formData.name" id="txtName" placeholder="姓名" />
+        <div class="form-group">
+          <label for="exampleInputPassword1">性别</label>
+          <!-- <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"> -->
+          <select id="gender" class="form-control" v-model="formData.gender">
+            <option value="男">男</option>
+            <option value="女">女</option>
+          </select>
+        </div>
       </div>
-      <button type="submit" class="btn btn-success">提交</button>
+      <button @click.prevent="add" class="btn btn-success">提交</button>
     </form>
   </div>
 </template>
 
 <script>
-export default {};
+//1.绑定文本框
+//2.点击按钮发送请求
+//3.添加成功，跳转会列表
+import axios from 'axios'
+export default {
+  data(){
+    return {
+      //封装表单数据
+      formData: {
+        name:'',
+        gender:'男'
+      }
+    }
+  },
+  methods:{
+    add(){
+      //点击按钮发送请求，添加
+      axios
+        .post('http://localhost:3000/heroes',this.formData)
+        .then((res) => {
+          const status = res.status
+          if(status === 201){
+            //添加成功
+            //添加成功，跳转回列表
+            this.$router.push('/hero')
+          }else {
+            alert('添加失败')
+          }
+        })
+        
+    }
+  }
+
+};
 </script>
 
 <style>
